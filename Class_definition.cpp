666665b5b7e34e88231c78da6b4438f9e1da55c3
Harshal Jain <harshaljn04@gmail.com>
2023-09-19 10:37:31 +0530

    Add files via upload

---
+++ Class_definition.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class person{
+    char name[30];
+    int age;
+    public:
+    void getdata()
+    {
+        cout<<"Enter name and age :-"<<endl;
+        cin>> name>> age;
+    };
+    void putdata()
+    {
+        cout<<name<<age<<endl;
+    }
+};
+
+int main()
+{
+    person p,p1;
+    p.getdata();
+    p1.getdata();
+    p.putdata();
+    p1.putdata();
+}
---
+++ First.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{ int a ;
+    //cout << "hello homies" ;
+    // cin>>a;
+    // cout << "output " <<a;
+    cin>>a;
+    cout<<sizeof(a);
+
+}
